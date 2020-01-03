d6fd696b34c91deac0bb387d981085bbef3d88d3
RJIII <rljames3212@gmail.com>
2020-01-02 21:28:07 -0600

    added helper functions to piece.go

--- board/location/location.go
+++ board/location/location.go
@@ -2,16 +2,20 @@ package location
 
 // Location represents a location on a chess board
 type Location struct {
-	row int
-	col int
+	Row int
+	Col int
 }
 
 // GetRow returns the row of the location
-func (l *Location) GetRow() int {
-	return l.row
+func (l Location) GetRow() int {
+	return l.Row
 }
 
 // GetCol returns the column of the location
-func (l *Location) GetCol() int {
-	return l.col
+func (l Location) GetCol() int {
+	return l.Col
+}
+
+func (l Location) Equals(other Location) bool {
+	return l.Row == other.Row && l.Col == other.Col
 }
--- pieces/piece.go
+++ pieces/piece.go
@@ -2,6 +2,31 @@ package pieces
 
 import "chess/board/location"
 
+const BOARD_SIZE int = 8
+
+func isValidLocation(l location.Location) bool {
+	return l.GetRow() >= 0 && l.GetRow() < BOARD_SIZE &&
+		l.GetCol() >= 0 && l.GetCol() < BOARD_SIZE
+}
+
+func isLocationOccupied(loc location.Location, pcs []*Piece) bool {
+	for _, p := range pcs {
+		if loc.Equals((*p).GetLocation()) {
+			return true
+		}
+	}
+	return false
+}
+
+func isLocationOccupiedByOpponent(loc location.Location, thisColor PieceColor, pcs []*Piece) bool {
+	for _, p := range pcs {
+		if (*p).GetColor() != thisColor && loc.Equals((*p).GetLocation()) {
+			return true
+		}
+	}
+	return false
+}
+
 // PieceColor is the color of a piece
 type PieceColor int32
 
