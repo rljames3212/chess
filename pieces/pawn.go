8e5bd3dc0e4a8df8fc916ca01e7888aa6c5cdf56
RJIII <rljames3212@gmail.com>
2020-01-03 19:23:48 -0600

    Created pawn class and corresponding tests

---
+++ pieces/pawn.go
@@ -0,0 +1,87 @@
+package pieces
+
+import "chess/board/location"
+
+// Pawn represents a chess pawn
+type Pawn struct {
+	loc      location.Location
+	hasMoved bool
+	color    PieceColor
+}
+
+const (
+	BLACK_DIRECTION int = -1
+	WHITE_DIRECTION int = 1
+)
+
+// NewPawn creates and returns a new pawn
+func NewPawn(l location.Location, pc PieceColor) *Pawn {
+	return &Pawn{
+		loc:      l,
+		color:    pc,
+		hasMoved: false,
+	}
+}
+
+// GetColor returns the color of the pawn
+func (p *Pawn) GetColor() PieceColor {
+	return p.color
+}
+
+// GetHasMoved returns whether or not the piece has moved
+func (p *Pawn) GetHasMoved() bool {
+	return p.hasMoved
+}
+
+// GetLocation returns the location of the pawn on the board
+func (p *Pawn) GetLocation() location.Location {
+	return p.loc
+}
+
+// GetValidMoves returns a slice of all moves the pawn can make given a piece configuration
+func (p *Pawn) GetValidMoves(pcs []Piece) []location.Location {
+	currentRow := p.loc.GetRow()
+	currentCol := p.loc.GetCol()
+
+	var validMoves []location.Location
+
+	var movementDirection int
+	if p.color == BLACK {
+		movementDirection = BLACK_DIRECTION
+	} else {
+		movementDirection = WHITE_DIRECTION
+	}
+
+	var loc location.Location
+	// check locations diagonally in front of pawn to see if a capture can be made
+	// check first diagonal
+	loc = location.Location{Row: currentRow + movementDirection, Col: currentCol + 1}
+	if isValidLocation(loc) && isLocationOccupiedByOpponent(loc, p.GetColor(), pcs) {
+		validMoves = append(validMoves, loc)
+	}
+	// check second diagonal
+	loc = location.Location{Row: currentRow + movementDirection, Col: currentCol - 1}
+	if isValidLocation(loc) && isLocationOccupiedByOpponent(loc, p.GetColor(), pcs) {
+		validMoves = append(validMoves, loc)
+	}
+	// check location directly in front of pawn
+	loc = location.Location{Row: currentRow + movementDirection, Col: currentCol}
+	if isValidLocation(loc) && !isLocationOccupied(loc, pcs) {
+		validMoves = append(validMoves, loc)
+	} else {
+		return validMoves
+	}
+
+	// if piece hasn't moved, check 2 spaces ahead of this piece
+	loc = location.Location{Row: currentRow + 2*movementDirection, Col: currentCol}
+	if !p.hasMoved && isValidLocation(loc) && !isLocationOccupied(loc, pcs) {
+		validMoves = append(validMoves, loc)
+	}
+	return validMoves
+}
+
+// Move sets the location of the pawn to a new location and sets hasMoved member to true
+func (p *Pawn) Move(newLocation location.Location) {
+	p.loc = newLocation
+	p.hasMoved = true
+}
---
+++ pieces/pieces_test.go
@@ -0,0 +1,182 @@
+package pieces
+
+import (
+	"chess/board/location"
+	"testing"
+)
+
+func TestUnmovedUnblockedBlackPawn(t *testing.T) {
+	loc := location.Location{Row: 7, Col: 1}
+	p := NewPawn(loc, BLACK)
+
+	var pcs []Piece
+
+	validMoves := p.GetValidMoves(pcs)
+	expectedMoves := []location.Location{
+		{Row: 6, Col: 1},
+		{Row: 5, Col: 1},
+	}
+
+	evaluate(validMoves, expectedMoves, t)
+}
+
+func TestUnmovedUnblockedWhitePawn(t *testing.T) {
+	loc := location.Location{Row: 1, Col: 1}
+	p := NewPawn(loc, WHITE)
+
+	var pcs []Piece
+
+	validMoves := p.GetValidMoves(pcs)
+	expectedMoves := []location.Location{
+		{Row: 2, Col: 1},
+		{Row: 3, Col: 1},
+	}
+
+	evaluate(validMoves, expectedMoves, t)
+}
+
+func TestUnmovedBlockedBlackPawn(t *testing.T) {
+	loc := location.Location{Row: 6, Col: 2}
+	p := NewPawn(loc, BLACK)
+	p1 := NewPawn(location.Location{Row: 5, Col: 2}, WHITE)
+	pcs := []Piece{p1}
+
+	validMoves := p.GetValidMoves(pcs)
+	var expectedMoves []location.Location
+
+	evaluate(validMoves, expectedMoves, t)
+}
+
+func TestUnmovedBlockedWhitePawn(t *testing.T) {
+	loc := location.Location{Row: 1, Col: 2}
+	p := NewPawn(loc, WHITE)
+	p1 := NewPawn(location.Location{Row: 2, Col: 2}, WHITE)
+	pcs := []Piece{p1}
+
+	validMoves := p.GetValidMoves(pcs)
+	var expectedMoves []location.Location
+
+	evaluate(validMoves, expectedMoves, t)
+}
+
+func TestMovedUnblockedBlackPawn(t *testing.T) {
+	p := NewPawn(location.Location{Row: 6, Col: 0}, BLACK)
+	p.Move(location.Location{Row: 5, Col: 0})
+
+	var pcs []Piece
+	validMoves := p.GetValidMoves(pcs)
+
+	expectedmoves := []location.Location{
+		{Row: 4, Col: 0},
+	}
+
+	evaluate(validMoves, expectedmoves, t)
+}
+
+func TestMovedUnblockedWhitePawn(t *testing.T) {
+	p := NewPawn(location.Location{Row: 1, Col: 0}, WHITE)
+	p.Move(location.Location{Row: 2, Col: 0})
+
+	var pcs []Piece
+	validMoves := p.GetValidMoves(pcs)
+
+	expectedmoves := []location.Location{
+		{Row: 3, Col: 0},
+	}
+
+	evaluate(validMoves, expectedmoves, t)
+}
+
+func TestMovedBlockedBlackPawn(t *testing.T) {
+	p := NewPawn(location.Location{Row: 7, Col: 0}, BLACK)
+	p.Move(location.Location{Row: 6, Col: 0})
+
+	pcs := []Piece{
+		NewPawn(location.Location{Row: 5, Col: 0}, BLACK),
+	}
+
+	validMoves := p.GetValidMoves(pcs)
+
+	var expectedMoves []location.Location
+
+	evaluate(validMoves, expectedMoves, t)
+}
+
+func TestMovedBlockedWhitePawn(t *testing.T) {
+	p := NewPawn(location.Location{Row: 1, Col: 0}, WHITE)
+	p.Move(location.Location{Row: 2, Col: 0})
+
+	pcs := []Piece{
+		NewPawn(location.Location{Row: 3, Col: 0}, BLACK),
+	}
+
+	validMoves := p.GetValidMoves(pcs)
+
+	var expectedMoves []location.Location
+
+	evaluate(validMoves, expectedMoves, t)
+}
+
+func TestCaptureBlackPawn(t *testing.T) {
+	p := NewPawn(location.Location{Row: 6, Col: 1}, BLACK)
+
+	pcs := []Piece{
+		NewPawn(location.Location{Row: 5, Col: 2}, WHITE),
+		NewPawn(location.Location{Row: 5, Col: 0}, BLACK),
+		NewPawn(location.Location{Row: 4, Col: 1}, WHITE),
+		NewPawn(location.Location{Row: 7, Col: 2}, WHITE),
+	}
+
+	validMoves := p.GetValidMoves(pcs)
+
+	expectedMoves := []location.Location{
+		{Row: 5, Col: 2},
+		{Row: 5, Col: 1},
+	}
+
+	evaluate(validMoves, expectedMoves, t)
+}
+
+func TestCaptureWhitePawn(t *testing.T) {
+	p := NewPawn(location.Location{Row: 1, Col: 1}, WHITE)
+
+	pcs := []Piece{
+		NewPawn(location.Location{Row: 2, Col: 2}, WHITE),
+		NewPawn(location.Location{Row: 2, Col: 0}, BLACK),
+		NewPawn(location.Location{Row: 4, Col: 1}, WHITE),
+		NewPawn(location.Location{Row: 7, Col: 2}, WHITE),
+	}
+
+	validMoves := p.GetValidMoves(pcs)
+
+	expectedMoves := []location.Location{
+		{Row: 2, Col: 0},
+		{Row: 2, Col: 1},
+		{Row: 3, Col: 1},
+	}
+
+	evaluate(validMoves, expectedMoves, t)
+}
+
+func evaluate(moves []location.Location, expectedMoves []location.Location, t *testing.T) {
+	t.Helper()
+
+	if len(moves) != len(expectedMoves) {
+		t.Errorf("ValidMoves length does not match ExpectedMoves length: %d = %d ", len(expectedMoves), len(moves))
+	}
+
+	for _, move := range moves {
+		if !contains(expectedMoves, move) {
+			t.Errorf("Unexpected move: %v", move)
+		}
+	}
+}
+
+func contains(moves []location.Location, loc location.Location) bool {
+	for _, move := range moves {
+		if loc.Equals(move) {
+			return true
+		}
+	}
+	return false
+}
